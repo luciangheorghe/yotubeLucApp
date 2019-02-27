70d233a2d619876ae241be46ff8dae3f9d176f47
Lucian Gheorghe <lucian.gheorghe1989@gmail.com>
2019-02-27 23:12:18 +0200

    html & scss - template created - layout started

--- assets/js/youtubeLucApp/App.js
+++ assets/js/youtubeLucApp/App.js
@@ -31,26 +31,160 @@ export default class App extends Component {
 
 	render() {
 		return (
-			<div className="container" style={{ marginTop: '1rem' }}>
-				<Header />
-				<VideoSearch getVideo={this.getVideo} />
-				{this.state.videos.map(video => {
-					console.log(video);
-					return (
-						<div key={video.id} className="col-sm-3">
-							<iframe
-								width="100%"
-								height="auto"
-								src={'https://www.youtube.com/embed/' + video.id}
-								frameBorder="0"
-								allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-								allowFullScreen
-							/>
-							<p>{video.id}</p>
-							<p>{video.snippet.title}</p>
+			<div className="container-fluid">
+				<div className="row main">
+					<div className="col-3 user-logo">
+						<div className="row logo">LucApp</div>
+						<div className="row user-info">
+							<div className="row user-pic-name">
+								<div className="user-pic">
+									<img src=".././img/luc-profile.jpg" alt="" />
+								</div>
+
+								<span className="user-name">Luc Gheorghe</span>
+								<div className="edit-profile">Edit Profile</div>
+							</div>
 						</div>
-					);
-				})}
+						<div className="options">
+							<div className="videos">
+								<i className="fab fa-youtube" />
+								<div className="text">Videos</div>
+							</div>
+							<div className="campaigns">
+								<i className="fas fa-baseball-ball" />
+								<div className="text">Campaigns</div>
+							</div>
+							<div className="favorites">
+								<i className="fas fa-star" />
+								<div className="text">Favorites</div>
+							</div>
+							<div className="wishlist">
+								<i className="fas fa-align-justify" />
+								<div className="text">Wishlist</div>
+							</div>
+							<div className="signout">
+								<i className="fas fa-sign-out-alt" />
+								<div className="text">Sign Out</div>
+							</div>
+						</div>
+					</div>
+
+					<div className="col-9 top-videos">
+						<div className="row header">
+							<Header />
+						</div>
+						<div className="row topVideo-tranding">
+							<div className="col-md-9 topVideo">
+								<h1 className="topVideos-title">Top Videos</h1>
+								<div className="videosContainer">
+									<div className="single-video">
+										<div className="video-thumbnail">
+											<img
+												src="https://i.ytimg.com/vi/qxWrnhZEuRU/mqdefault.jpg"
+												alt=""
+											/>
+										</div>
+										<p className="video-title">Title</p>
+										<span className="video-author"> author </span>
+									</div>
+									<div className="single-video">
+										<div className="video-thumbnail">
+											<img
+												src="https://i.ytimg.com/vi/qxWrnhZEuRU/mqdefault.jpg"
+												alt=""
+											/>
+										</div>
+										<p className="video-title">Title</p>
+										<span className="video-author"> author </span>
+									</div>
+									<div className="single-video">
+										<div className="video-thumbnail">
+											<img
+												src="https://i.ytimg.com/vi/qxWrnhZEuRU/mqdefault.jpg"
+												alt=""
+											/>
+										</div>
+										<p className="video-title">Title</p>
+										<span className="video-author"> author </span>
+									</div>
+									<div className="single-video">
+										<div className="video-thumbnail">
+											<img
+												src="https://i.ytimg.com/vi/qxWrnhZEuRU/mqdefault.jpg"
+												alt=""
+											/>
+										</div>
+										<p className="video-title">Title</p>
+										<span className="video-author"> author </span>
+									</div>
+									<div className="single-video">
+										<div className="video-thumbnail">
+											<img
+												src="https://i.ytimg.com/vi/qxWrnhZEuRU/mqdefault.jpg"
+												alt=""
+											/>
+										</div>
+										<p className="video-title">Title</p>
+										<span className="video-author"> author </span>
+									</div>
+									<div className="single-video">
+										<div className="video-thumbnail">
+											<img
+												src="https://i.ytimg.com/vi/qxWrnhZEuRU/mqdefault.jpg"
+												alt=""
+											/>
+										</div>
+										<p className="video-title">Title</p>
+										<span className="video-author"> author </span>
+									</div>
+									<div className="single-video">
+										<div className="video-thumbnail">
+											<img
+												src="https://i.ytimg.com/vi/qxWrnhZEuRU/mqdefault.jpg"
+												alt=""
+											/>
+										</div>
+										<p className="video-title">Title</p>
+										<span className="video-author"> author </span>
+									</div>
+									<div className="single-video">
+										<div className="video-thumbnail">
+											<img
+												src="https://i.ytimg.com/vi/qxWrnhZEuRU/mqdefault.jpg"
+												alt=""
+											/>
+										</div>
+										<p className="video-title">Title</p>
+										<span className="video-author"> author </span>
+									</div>
+									<div className="single-video">
+										<div className="video-thumbnail">
+											<img
+												src="https://i.ytimg.com/vi/qxWrnhZEuRU/mqdefault.jpg"
+												alt=""
+											/>
+										</div>
+										<p className="video-title">Title</p>
+										<span className="video-author"> author </span>
+									</div>
+									<div className="single-video">
+										<div className="video-thumbnail">
+											<img
+												src="https://i.ytimg.com/vi/qxWrnhZEuRU/mqdefault.jpg"
+												alt=""
+											/>
+										</div>
+										<p className="video-title">Title</p>
+										<span className="video-author"> author </span>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-3 whatstranding" />
+						</div>
+					</div>
+				</div>
+
+				<VideoSearch getVideo={this.getVideo} />
 			</div>
 		);
 	}
--- assets/js/youtubeLucApp/components/VideoSearch.js
+++ assets/js/youtubeLucApp/components/VideoSearch.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const VideoSearch = props => (
 	<form onSubmit={props.getVideo}>
-		<input type="text" name="videoInfo" />
-		<button>Search</button>
+		{/* <input type="text" name="videoInfo" />
+		<button>Search</button> */}
 	</form>
 );
 
